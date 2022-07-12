554f9ae0a125929e1fe479651b34ef9422bb7bb6
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-12 16:08:15 -0600

    Spec: wrote test for merchant items index api request

--- spec/requests/v1/merchants/items/index_request_spec.rb
+++ spec/requests/v1/merchants/items/index_request_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe "The Item Index API" do
   it "sends a list of a merchants items as a response" do
-    create_list(:item, 3)
-
+    create_list(:merchant, 3)
+    
+    
     get api_v1_merchant_items_path(Merchant.last.id)
     
     expect(response).to be_successful
