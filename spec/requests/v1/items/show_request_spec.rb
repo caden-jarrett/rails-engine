9c832914e455abf1b95d6c1543be8cf979170dab
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-13 14:26:43 -0600

    Spec: added sad path testing for items show API andpoint

--- spec/requests/v1/items/show_request_spec.rb
+++ spec/requests/v1/items/show_request_spec.rb
@@ -1,22 +1,34 @@
 require 'rails_helper'
 
 describe "The Item Show API" do
-  it "sends a single item as a response" do
-    create_list(:merchant, 1)
+    describe 'happy path' do
+        it "sends a single item as a response" do
+            create_list(:merchant, 1)
 
-    get api_v1_item_path(Item.last.id)
-    
-    expect(response).to be_successful
+            get api_v1_item_path(Item.last.id)
+            
+            expect(response).to be_successful
 
-    item = JSON.parse(response.body, symbolize_names: true)
+            item = JSON.parse(response.body, symbolize_names: true)
 
-  
-    expect(item[:data]).to have_key(:id)
-    expect(item[:data][:id]).to eq(Item.last.id.to_s)
-    expect(item[:data][:id]).to be_a(String)
-    expect(item[:data][:attributes]).to have_key(:merchant_id)
-    expect(item[:data][:attributes][:name]).to be_an(String)
-    expect(item[:data][:attributes][:description]).to be_an(String)
-    expect(item[:data][:attributes][:unit_price]).to be_an(Float)
-  end
+        
+            expect(item[:data]).to have_key(:id)
+            expect(item[:data][:id]).to eq(Item.last.id.to_s)
+            expect(item[:data][:id]).to be_a(String)
+            expect(item[:data][:attributes]).to have_key(:merchant_id)
+            expect(item[:data][:attributes][:name]).to be_an(String)
+            expect(item[:data][:attributes][:description]).to be_an(String)
+            expect(item[:data][:attributes][:unit_price]).to be_an(Float)
+        end
+    end
+
+    describe 'sad path' do
+        it "sends a 404 error as a response" do
+            create_list(:merchant, 1)
+
+            get api_v1_item_path(10000000000000000000000000000001)
+            
+            expect(response.status).to eq(404)
+        end
+    end
 end 
