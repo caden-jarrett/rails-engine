cca244836dace7bd6923969838a16cfe98e12729
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-14 13:49:34 -0600

    Spec: added validations to item

--- spec/models/item_sepc.rb
+++ spec/models/item_sepc.rb
@@ -6,4 +6,11 @@ RSpec.describe Item, type: :model do
     it { should have_many(:invoice_items)}
     it { should have_many(:items).through(:invoice_items)}
   end
+
+  describe 'validations' do
+    it_validates_prescence_of :name
+    it_validates_prescence_of :description
+    it_validates_prescence_of :unit_price
+    it_validates_prescence_of :merchant_id
+  end
 end
