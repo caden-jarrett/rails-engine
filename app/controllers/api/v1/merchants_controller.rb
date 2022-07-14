c39ac0554529a6efaae4d437e7a5a635c2f064b5
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-14 14:35:25 -0600

    Spec: added request spec for merchant search API endpoint

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,10 +1,12 @@
 
 class Api::V1::MerchantsController < ApplicationController
   def index
+    binding.pry
     render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show 
+    binding.pry
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 end
---
+++ spec/requests/v1/merchants/find_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "The Merchant Search API" do
+  it "sends a single merchant as a response" do
+    create_list(:merchant, 1, name: "Aaron Inc")
+    create_list(:merchant, 1, name: "Todd Inc")
+    create_list(:merchant, 1, name: "Peter Inc")
+    create_list(:merchant, 1, name: "Aaron Inc")
+
+    get '/api/v1/merchants/find', params: { name: 'aa'}
+    
+    expect(response).to be_succesful
+  end
+end 
