35a46c331c30f359b13998cd47777628e3dbe46f
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-14 14:09:05 -0600

    Spec: added validations for invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,6 @@ class Invoice < ApplicationRecord
     has_many :transactions
     has_many :invoice_items
     has_many :item, through: :invoice_items
+
+    validates_presence_of :customer_id, :merchant_id, :status
 end
