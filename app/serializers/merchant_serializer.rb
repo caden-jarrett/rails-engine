76c64d331c84b734928e74424796a65a9b044e28
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-14 16:11:10 -0600

    Docs: added serializer method, and controller method to expose find merchant API endpoint

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,4 +1,8 @@
 class MerchantSerializer
   include JSONAPI::Serializer
   attributes :name
+
+  def self.no_match
+    {data: {}, errors: 'expected null to be an object'}
+  end
 end
--- spec/requests/v1/merchants/find_request_spec.rb
+++ spec/requests/v1/merchants/find_request_spec.rb
@@ -9,6 +9,28 @@ describe "The Merchant Search API" do
 
     get '/api/v1/merchants/find', params: { name: 'aa'}
     
-    expect(response).to be_succesful
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+
+    expect(response.status).to eq(200)
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
+
+  describe 'sad path' do
+        it "sends a single merchant as a response" do
+            create_list(:merchant, 1, name: "Aaron Inc")
+            create_list(:merchant, 1, name: "Todd Inc")
+            create_list(:merchant, 1, name: "Peter Inc")
+            create_list(:merchant, 1, name: "Patrick Inc")
+
+            get '/api/v1/merchants/find', params: { name: '$$'}
+            
+            merchant = JSON.parse(response.body, symbolize_names: true)
+
+
+            expect(merchant[:errors]).to eq("expected null to be an object")
+        end
+    end
 end 
