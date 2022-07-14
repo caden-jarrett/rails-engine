c3305f3ef7783ec4ffc998a6815891e3853c2171
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-14 13:50:57 -0600

    Docs: added validations to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
     belongs_to :merchant
     has_many :invoice_items
     has_many :invoices, through: :invoice_items
+
+    validates_prescence_of :name, :description, :unit_price, :merchant_id
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many(:items)}
   end
+
+   describe 'validations' do
+    it_validates_prescence_of :name
+  end
 end
